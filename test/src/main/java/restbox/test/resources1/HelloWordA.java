00e79ef45ec38a5616a43fe85405aef42eca3acf
SRK.Lyu <superalsrk@gmail.com>
2014-08-02 23:59:16 +0800

    related test project

--- test/src/main/java/restbox/test/resources1/HelloWordA.java
+++ test/src/main/java/restbox/test/resources1/HelloWordA.java
@@ -4,13 +4,15 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.HttpMethod;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/test")
 public class HelloWordA {
 	
-	@GET@Produces(MediaType.TEXT_PLAIN)
-	public String sayHello(){
-		return "hello world";
+	@GET@Path("/hi")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String sayHello(@QueryParam("name") String name){
+		return "hello " + name;
 	}
 }
